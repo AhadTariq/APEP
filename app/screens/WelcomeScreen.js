78265a92aae0f3a96a191b329d5a72b61e840b93
Ahad Tariq <CaptainXAhad@outlook.com>
2020-11-18 20:40:44 +0500

    new

--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -42,7 +42,7 @@ const styles = StyleSheet.create({
     },
     tagline: {
         fontSize: 25,
-        fontWeight: "600",
+        fontWeight: "800",
         paddingVertical:20,
         textAlign: "center",
     },
