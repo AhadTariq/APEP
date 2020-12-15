1b42ccb7d2ef6adabf3fbb806e2df6407d18dac6
Ahad Tariq <CaptainXAhad@outlook.com>
2020-12-15 23:45:12 +0500

    refactoring little bit

--- app/components/ListItems.js
+++ app/components/ListItems.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import { View, StyleSheet, Image, TouchableHighlight } from 'react-native';
+
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 import Swipeable from 'react-native-gesture-handler/Swipeable';
+
 import colors from '../config/colors';
 import AppText from './AppText';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
 
 function ListItems({title, subTitle, image,ImageComponent, onPress, renderRightActions}) {
     return (
@@ -14,9 +18,10 @@ function ListItems({title, subTitle, image,ImageComponent, onPress, renderRightA
         {ImageComponent}
         {image && <Image style={styles.image} source={image}/>}
         <View style={styles.detailsContainer}>
-            <AppText style={styles.title}>{title}</AppText>
-            {subTitle && <AppText style={styles.subTitle}>{subTitle}</AppText>}
+            <AppText style={styles.title} numberOfLine={1}>{title}</AppText>
+            {subTitle && <AppText style={styles.subTitle} numberOfLine={2}>{subTitle}</AppText>}
         </View>
+        <MaterialCommunityIcons color={colors.Lightgrey} name="chevron-right" size={25}/>
      </View>
     </TouchableHighlight>
     </Swipeable>
@@ -25,11 +30,13 @@ function ListItems({title, subTitle, image,ImageComponent, onPress, renderRightA
 
 const styles = StyleSheet.create({
     container: {
+        alignItems: "center",
         flexDirection: "row",
         padding: 15,
         backgroundColor: colors.white,
     },
     detailsContainer: {
+        flex: 1,
         marginLeft: 10,
         justifyContent: "center",
     },
