b8f6781e3b10301deeb8e103c5d4f29e7bfe6b15
Ahad Tariq <CaptainXAhad@outlook.com>
2020-12-17 17:37:18 +0500

    Creating permssions module

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import {MaterialCommunityIcons} from "@expo/vector-icons";
 
 import Screen from './app/components/Screen'
 import { StyleSheet, Text, View, Image, SafeAreaView, Button, Alert, Dimensions, TextInput, Switch } from 'react-native';
 import Icon from './app/components/Icon';
 import ListItem from './app/components/ListItems';
+
+
+import * as ImagePicker from 'expo-image-picker';
+import * as Permissions from 'expo-permissions';
 
 import ViewImageScreen from './app/screens/ViewImageScreen';
 import WelcomeScreen from './app/screens/WelcomeScreen';
@@ -24,8 +28,22 @@ import SignupScreen from './app/screens/SignupScreen';
 import ListingEditScreen from './app/screens/ListingEditScreen';
 export default function App() {
   
+  const requestPermission = async () => {
+    const result = await Permissions.askAsync(Permissions.CAMERA_ROLL, Permissions.LOCATION)
+    result.granted
+
+
+    const { granted } = await ImagePicker.requestCameraRollPermissionsAsync();
+    if(!granted)
+      alert('You need to anable permission to access the library');
+  }
+
+  useEffect(() => {
+    requestPermission();
+  }, [])
+
   return(
-   <ListingEditScreen/>
+   <WelcomeScreen/>
   )
 }
 
