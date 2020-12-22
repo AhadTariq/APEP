c273dc2407faa92ebc067301b404e4ccb16b5137
Ahad Tariq <CaptainXAhad@outlook.com>
2020-12-23 02:44:00 +0500

    ref hook with scrilling view in imageimnput

--- app/components/ImageInputList.js
+++ app/components/ImageInputList.js
@@ -1,9 +1,13 @@
-import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import React, {useRef} from 'react';
+import { View, StyleSheet, ScrollView } from 'react-native';
 import ImageInput from './ImageInput';
 
 function ImageInputList({ imageUris = [], onRemoveImage, onAddImage }) {
+   const scrollView = useRef();
+    
+
     return (
+        <ScrollView ref={scrollView} horizontal onContentSizeChange={()=> scrollView.current.scrollToEnd()}>
        <View style={styles.container}>
            {imageUris.map(uri =>(
                <View key={uri} style={styles.image}>
@@ -14,6 +18,7 @@ function ImageInputList({ imageUris = [], onRemoveImage, onAddImage }) {
            ))}
            <ImageInput onChangeImage={(uri) => onAddImage(uri)}/>
        </View>
+       </ScrollView>
     );
 }
 
