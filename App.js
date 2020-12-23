2c44839a456756aa7e5b3b5af38c7cea5536355d
Ahad Tariq <CaptainXAhad@outlook.com>
2020-12-23 07:32:40 +0500

    Completing navigator with icons

--- App.js
+++ App.js
@@ -16,7 +16,7 @@ import * as Permissions from 'expo-permissions';
 
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer, useNavigation } from '@react-navigation/native';
-
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import ViewImageScreen from './app/screens/ViewImageScreen';
 import WelcomeScreen from './app/screens/WelcomeScreen';
@@ -47,28 +47,63 @@ const navigation = useNavigation();
 const Faculty = ({navigation }) => (
   <Screen>
     <Text>Faculty</Text>
-    <Link/>
+    <Button
+      title="View Faculty"
+      onPress={()=> navigation.navigate("FacultyDetails", {id: 1})}
+    />
   </Screen>
 );
 
-const FacultyDetails = () => (
+const FacultyDetails = ({route}) => (
   <Screen>
-    <Text>FacultyDetails</Text>
+    <Text>FacultyDetails {route.params.id}</Text>
   </Screen>
 );
 
 const Stack = createStackNavigator();
 const StackNavigator = () => (
-  <Stack.Navigator>
-    <Stack.Screen name="Faculty" component={Faculty}/>
-    <Stack.Screen name="FacultyDetails" component={FacultyDetails}/>
+  <Stack.Navigator 
+    screenOptions={{
+      headerStyle: { backgroundColor: "dodgerblue" },
+      headerTintColor: "white",
+    }}
+  >  
+    <Stack.Screen name="Faculty" component={Faculty} options={{
+      headerStyle: { backgroundColor: 'tomato' },
+      headerTintColor: "white",
+      headerShown: false,
+    }} />
+    <Stack.Screen name="FacultyDetails" component={FacultyDetails}
+      options={({route})=>({title:route.params.id})}
+    />
   </Stack.Navigator>
 );
+
+const Account = () => <Screen><Text>Account</Text></Screen>
+
+const Tab = createBottomTabNavigator()
+const TabNavigator = () => (
+  <Tab.Navigator 
+      tabBarOptions={{
+        activeBackgroundColor: "tomato",
+        activeTintColor: "white",
+        inactiveBackgroundColor: '#eee',
+        inactiveTintColor: 'black'
+      }}>
+    <Tab.Screen name="Feed" component={StackNavigator} 
+    // options={{
+    //   tabBarIcon: ({ size, color }) => <MaterialCommunityIcons name="home" size={size} color={color}/>
+    // }}
+    />
+    <Tab.Screen name="Account" component={Account}/>
+  </Tab.Navigator>
+)
+
 export default function App() {
  
   return(
   <NavigationContainer>
-    <StackNavigator/>
+    <TabNavigator/>
   </NavigationContainer>
   )
 }
